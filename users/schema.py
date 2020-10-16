bb61909b412acba921b7e173e7b221e36eeaabe7
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-10-16 14:27:19 +0900

    fianl

--- users/schema.py
+++ users/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from .types import UserType
-from .mutations import CreateAccountMutation, LoginMutation
-from .queries import resolve_user
+from .mutations import CreateAccountMutation, LoginMutation, ToggleFavsMutation, EditProfileMutation
+from .queries import resolve_user, resolve_me
 
 
 class Query(object):
@@ -10,8 +10,12 @@ class Query(object):
                           id=graphene.Int(required=True),
                           resolver=resolve_user)
 
+    me = graphene.Field(UserType, resolver=resolve_me)
+
 
 class Mutation(object):
 
     create_account = CreateAccountMutation.Field()
     login = LoginMutation.Field()
+    toggle_favs = ToggleFavsMutation.Field()
+    edit_profile = EditProfileMutation.Field()
